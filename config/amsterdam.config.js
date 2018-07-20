45b1aab2e828ea74381b21d1e70345e532dd428c
e.jager@amsterdam.nl <info@lytrix.com>
2018-07-20 10:23:37 +0200

    added subdomains and wms service

--- config/amsterdam.config.js
+++ config/amsterdam.config.js
@@ -1,97 +1,84 @@
 export default {
-    "version": 0.1,    
+    "version": 0.2,
     "basemaps": {
         "defaults": {
-            "crs": "EPSG:3857",
-            "attr": "Kaartgegevens &copy; <a href='https://www.kadaster.nl'>Kadaster</a> | <a href='https://www.verbeterdekaart.nl'>Verbeter de kaart</a>",
-            "minZoom": 6,
-            "maxZoom": 19,
-            "type": "wmts",
-            "format": "jpeg",
-            "url": "https://geodata.nationaalgeoregister.nl/tiles/service"
+            "attribution": "Kaartgegevens CC-BY-4.0 Gemeente Amsterdam",
+            "minZoom": 12,
+            "maxZoom": 21,
+            "type": "tms",
+            "format": "png",
+            "subdomains": ['t1', 't2', 't3', 't4'],
+            "url": "https://{s}.data.amsterdam.nl"
         },
         "layers": [
             {
                 "name": "standaard",
-                "urlname": "brtachtergrondkaart"                
+                "layerName": "topo_wm"
             },
             {
-                "name": "grijs",
-                "urlname": "brtachtergrondkaartgrijs"
+                "name": "zwartwit",
+                "layerName": "topo_wm_zw"
             },
             {
-                "name": "pastel",
-                "urlname": "brtachtergrondkaartpastel"
-            },{
-                "name": "luchtfoto",
-                "urlname": "2016_ortho25",
-                "url": "https://geodata.nationaalgeoregister.nl/luchtfoto/rgb",
-                "format": "jpeg"
+                "name": "licht",
+                "layerName": "topo_wm_light"
+            },
+            {
+                "name": "donker",
+                "layerName": "topo_wm"
             }
         ]
     },
     "wms": {
         "defaults": {
-            "url": "https://geodata.nationaalgeoregister.nl/{workSpaceName}/wms?",            
-            "version": "1.1.1",
+            "name": "stadsdeel",
+            "url": "https://map.data.amsterdam.nl/maps/gebieden",
+            "layerName": "stadsdeel",
+            "version": "1.1.0",
             "transparent": true,
             "format": "image/png",
             "minZoom": 0,
-            "maxZoom": 24
+            "maxZoom": 24,
+            "styleName": ""
         },
         "layers": [
-            {
-                "name": "foobar",
-                "workSpaceName": "bash",
-                "layerName": "peachy"
-            },
-            {
-                "name": "gebouwen",
-                "workSpaceName": "bag",
-                "layerName": "pand"
-            },
-            {
-                "name": "percelen",
-                "workSpaceName": "bkadastralekaartv3ag",
-                "layerName": "kadastralekaart"
-            },
-            {
-                "name": "drone-no-fly-zones",
-                "workSpaceName": "dronenoflyzones",
-                "layerName": "luchtvaartgebieden,landingsite"
-            },
-            {
-                "name": "hoogte",
-                "workSpaceName": "ahn2",
-                "layerName": "ahn2_05m_int",
-                "styleName": "ahn2:ahn2_05m_detail"
-            },
-            {
-                "name": "gemeenten",
-                "workSpaceName": "bestuurlijkegrenzen",
-                "layerName": "gemeenten",
-                "styleName": "bestuurlijkegrenzen:bestuurlijkegrenzen_gemeentegrenzen"
-            },
-            {
-                "name": "provincies",
-                "workSpaceName": "bestuurlijkegrenzen",
-                "layerName": "provincies",
-                "styleName": "bestuurlijkegrenzen:bestuurlijkegrenzen_provinciegrenzen"
+            {   
+                "name": "stadsdeel",
+                "layerName": "stadsdeel",
+                "url": "https://map.data.amsterdam.nl/maps/gebieden",
+                "version": "1.1.0"
             }
         ]
     },
     "geocoder": {
-        "suggestUrl": "https://geodata.nationaalgeoregister.nl/locatieserver/v3/suggest?",
-        "lookupUrl": "https://geodata.nationaalgeoregister.nl/locatieserver/v3/lookup?"
+        "suggestUrl": "https://geodata.nationaalgeoregister.nl/locatieserver/v3/suggest?fq=gemeentenaam:amsterdam&",
+        "lookupUrl": "https://geodata.nationaalgeoregister.nl/locatieserver/v3/lookup?fq=gemeentenaam:amsterdam&"
+    },
+    "featureQuery": {
+        "baseUrl": "https://api.data.amsterdam.nl/bag/nummeraanduiding/?format=json&locatie=",
+    },
+    "marker" : {
+      "url": 'https://map.data.amsterdam.nl/dist/images/svg/marker.svg',
+      "iconSize": [40,40],
+      "iconAnchor": [20, 39]
     },
     "map": {
         "style": 'standaard',
         "center": {
-            "latitude": 52.093249,
-            "longitude": 5.111994
+            "latitude": 52.2,
+            "longitude": 4.8952
         },
-        "zoom": 8,
+        "zoom": 11,
         "attribution": true,
-        "extent": [-180,-90,180,90]
+        "extent":  [ 52.25168, 4.64034, 52.50536, 5.10737 ],
+        "zoomposition": "bottomright"
+    },
+    "classnames": {
+        'geocoderContainer': ['embed-search'],
+        'geocoderSearch': ['invoer'],
+        'geocoderButton': ['primary','action','embed-search__button'],
+        'geocoderResultList': ['embed-search__auto-suggest'],
+        'geocoderResultItem' : ['embed-search__auto-suggest__item'],
+        'geocoderResultSelected' : ['embed-search__auto-suggest__item--active']
     }
 }
